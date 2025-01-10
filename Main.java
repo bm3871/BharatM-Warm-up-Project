5a9dfd2ae8a967b3850cff64290b08998110ec94
bm3871 <bm3871@pleasantonusd.net>
2025-01-10 22:56:56 +0000

    final commit

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int max = 1000;
         int sum = 0;
 
-        for(int i = 0; i < max; i++) {
+        for(int i = 0; i < 1000; i++) {
             if (i % 3 ==0 || i % 5 == 0) {
                 sum += i;
             }
