9e7e020ce1f44922253e80981b75a4a3c8d36b71
bm3871 <bm3871@pleasantonusd.net>
2025-01-10 22:45:22 +0000

    final commit

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int max = 1000;
+        int sum = 0;
+
+        for(int i = 0; i < max; i++) {
+            if (i % 3 ==0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println("The sum of all multiples of 3 or 5 below 1000 is " + sum);
     }
 }
